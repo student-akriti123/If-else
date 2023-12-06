dd7c1decdb53b908b2b1a31ecf6fb500fd03781b
student-akriti123 <akritisinha1602@gmail.com>
2023-12-06 10:50:28 +0530

    first commit

---
+++ odd even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a;
+ printf("Enter the value of a\n");
+ scanf("%d",&a);
+
+ if(a%2==0)
+    {
+        printf("This is even number\n");
+    }
+   else
+    {
+       printf("This is odd number\n");
+    }
+
+    return 0;
+}
