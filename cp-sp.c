65d91e8a22c13c0d8ee7d1ddfd874f36cdfddab7
student-akriti123 <akritisinha1602@gmail.com>
2023-12-06 11:32:25 +0530

    first commit

---
+++ cp-sp.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+ int sp;
+ printf("Enter the value of sp\n");
+ scanf("%d",&sp);
+
+ int cp;
+ printf("Enter the value of cp\n");
+ scanf("%d",&cp);
+
+ if(sp>cp)
+    {
+       printf("profit\n");
+    }
+
+   else
+    {
+       printf("loss\n");
+    }
+
+ return 0;
+}
